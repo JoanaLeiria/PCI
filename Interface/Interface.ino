09b8698c9b4c8c789c292818050a12ca05659c92
jfs2000 <87146207+jfs2000@users.noreply.github.com>
2021-07-09 12:05:05 +0100

    Import das bibliotecas e descricao do algoritmo

--- Interface/Interface.ino
+++ Interface/Interface.ino
@@ -32,7 +32,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 #include <Stepper.h>
 const int stepsPerRevolution = 100;  // change this to fit the number of steps per revolution for your motor 2038
 // inicializa o motor para as entradas digitais 2,3,4 e 5
-Stepper myStepper(stepsPerRevolution, 2, 4, 3, 5);
+Stepper motor(stepsPerRevolution, 2, 4, 3, 5);
 //fim stepper ----------------------
 
 //IR REMOTE
@@ -51,6 +51,11 @@ int button3 = 12;
 // Pino do sensor
 const int LM35 = A3;
 
+//Condicoes extra e variaveis de controlo
+int N = 10;  // numero de bits do arduino
+bool overTemperature = false;
+const int maxTemp = 80;  // temperatura maxima: 80 ºC
+
 void setup() {
     /* Comecar por definir o modo dos pinos */
     //os pinos dos botoes sao de input
@@ -67,7 +72,37 @@ void setup() {
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+    /*  O fluxo e':
+        1. esperar por input do comando, que pode ser:
+            a. lavar ja'
+                1. esperar que a porta da maquina esteja fechada (botao1 pressed)
+                2. esperar por input do tipo de programa (comando)
+                3. iniciar lavagem
+                    1. periodicamente verificar a temperatura;
+                        1. se estiver a sobre aquecer, parar lavagem
+                    2. se botao de pause clicado, parar a lavagem
+                4. enviar mensagem para o display de lavagem concluida ou nao
+
+            b. por um temporizador
+                1. esperar que a porta da maquina esteja fechada (botao1 pressed) (por seguranca)
+                2. esperar por input do tipo de programa (comando)
+                3. por em wait
+                4. SE a porta continua fechada (por seguranca), iniciar lavagem
+                    1. periodicamente verificar a temperatura;
+                        1. se estiver a sobreaquecer, parar lavagem
+                    2. se botao de pause clicado, parar a lavagem
+                5. enviar mensagem para o display de mensagem concluida ou nao
+
+            c. opcoes de manutecao
+                1. esperar que a porta da maquina esteja fechada (botao1 pressed)
+                2. inicar operacao de manutencao
+                    1. periodicamente verificar a temperatura;
+                        1. se estiver a sobreaquecer, parar
+                    2. se botao de pause clicado, parar
+                3. enviar mensagem para o display de mensagem concluida ou nao
+        
+        OBS: passos intermedios tambem podem ter mensagens no display
+    */
 }
 
 // *************************
@@ -90,7 +125,7 @@ void selectProgram() {
 }
 
 /* Mensagens para o display */
-void dispMessages() {
+void dispMessages(String message) {
 }
 
 /* Funcao que espera que se clique num botao*/
@@ -121,41 +156,60 @@ void startWashing(String desiredProgram) {
     switch (desiredProgram) {
         case "Eco":
             motorSpeed = 10;
-            cycleTime = ;
+            cycleTime = 10;
+            break;
+        case "Intensivo":
+            break;
+        case "Diario":
+            break;
+        case "Suave":
+            break;
+        case "Rapido":
             break;
     }
-    case "":
-        break;
-}
 
-/* Funcao para parar o programa de lavagem*/
-void stopWashing() {
-    // instrucoes para parar o motor
-    // e imprimir no display mensagem stopped
+    /* Funcao para parar o programa de lavagem*/
+    void stopWashing() {
+        // instrucoes para parar o motor
+        // e imprimir no display mensagem stopped
+        motor.setSpeed(0);
+        motor.step(0);
+    }
 }
-
 /* Funcao para verificar o nivel de calcario */
 /* ....*/
 
 /* Funcao para verificar a temperatura */
-/*
-  2 opcoes:
+/* 2 opcoes:
   verificamos se esta' a uma temperatura maior do que uma pre-definida
   ... ou 
   verificamos se aumentou muito desde a ultima vez que verificámos:
   - se sim, e' melhor aumentar a taxa com que medimos a temperatura
   - se nao, podemos baixar
 
+- variavel delta_t;
+- ou entao, se for do tipo temperatura=0.8*maxTemp, repetir leitura 
+
   OBS: ainda nao sei se faz sentido este metodo ser void ou outra coisa
 */
 void checkTemperature(int lastCheck) {
     int ADC_read = analogRead(LM35);
-    int temperature = ADC_read * Vs / (pow(2, N) - 1);
+    int temperature = ADC_read * Vs / (pow(2, N) - 1);  // N e' o numero de bits do ADC
 
-    if (temperature > lastCheck) {
-        /* break variable*/
+    if (temperature >= maxTemp) {
+        // sobre aquecimento; parar o motor
+        overTemperature = true;  // break variable
     }
 }
 
+/*  COMANDO:
+    Para o comando estamos a pensar fazer a seguinte correspondencia entre
+    botao (do telecomando) e operacao (da maquina)
+    
+    { CH-, CH, CH+ } -> operacoes de manutencao: abrilhantador, ....
+    { PREV, NEXT } -> visualizar os diferentes programas
+    { PLAY/PAUSE } -> comecar e parar lavagem
+    { EQ } -> anular qualquer operacao que esteja a decorrer ou que esteja programada
 
-
+    botao para propriamente ligar/desligar a maquina?
+*/
